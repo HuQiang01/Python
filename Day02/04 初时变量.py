d8d759c6e6dd2764abbe06ece30a84930af79ac3
Administrator <137628344@qq.com>
2023-11-23 13:10:10 +0800

    add 04 05 06 07 08

---
+++ Day02/04 初时变量.py
@@ -0,0 +1,12 @@
+#
+# print(1 + 2)
+# print(1 - 2)
+# print(1 * 2)
+# print(1 / 2)
+x = 1
+y = 2
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+
---
+++ Day02/05 变量值修复.py
@@ -0,0 +1,11 @@
+x = 10
+print(x)
+x = 20
+print(x)
+
+player_name = "后羿"
+player_level = 1
+print(player_name, player_level)
+print("闯关成功！！！")
+player_level = 3
+print(player_name, player_level)
