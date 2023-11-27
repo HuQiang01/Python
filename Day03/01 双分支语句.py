25f9ed45ee9d2b24f398bdd4daa5e8a9e76eb4ab
Administrator <137628344@qq.com>
2023-11-27 13:06:03 +0800

    add Day03-01

---
+++ Day03/01 双分支语句.py
@@ -0,0 +1,22 @@
+"""
+if 表达式:
+    语句1:
+    语句2:
+else:
+    语句3:
+    语句4:
+"""
+x = 1
+y = 2
+if x > y:
+    print("x > y")
+else:
+    print("x < y")
+
+age = int(input("请输入你的年龄："))
+
+if age >= 18:
+    print("你已经成年了")
+else:
+    print("你还是一个小孩子")
+print("年龄信息确认")
